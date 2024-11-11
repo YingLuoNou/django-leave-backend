ef0f250d427b05cd6c42b1558d5cb5355f79e043
eh <162614736+zsyeh@users.noreply.github.com>
2024-11-11 11:08:21 +0800

    更新 signals.py
    优化了信号处理

--- leave/signals.py
+++ leave/signals.py
@@ -1,30 +1,31 @@
-# leave/signals.py
-
+from django.db import transaction
 from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from .models import StudentProfile, TeacherProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        # 检查用户所属的组并创建相应的 Profile
-        if instance.groups.filter(name='stu').exists():
-            StudentProfile.objects.create(user=instance)
-        elif instance.groups.filter(name='tch').exists():
-            TeacherProfile.objects.create(user=instance)
+        with transaction.atomic():
+            if instance.groups.filter(name='stu').exists():
+                StudentProfile.objects.create(user=instance)
+            elif instance.groups.filter(name='tch').exists():
+                TeacherProfile.objects.create(user=instance)
 
 @receiver(m2m_changed, sender=User.groups.through)
-def add_user_profile(sender, instance, action, **kwargs):
+def manage_user_profile(sender, instance, action, reverse, model, pk_set, **kwargs):
     if action == 'post_add':
-        for group in instance.groups.all():
-            if group.name == 'stu' and not hasattr(instance, 'studentprofile'):
-                StudentProfile.objects.create(user=instance)
-            elif group.name == 'tch' and not hasattr(instance, 'teacherprofile'):
-                TeacherProfile.objects.create(user=instance)
+        with transaction.atomic():
+            for group in instance.groups.filter(pk__in=pk_set):
+                if group.name == 'stu' and not hasattr(instance, 'studentprofile'):
+                    StudentProfile.objects.create(user=instance)
+                elif group.name == 'tch' and not hasattr(instance, 'teacherprofile'):
+                    TeacherProfile.objects.create(user=instance)
     elif action == 'post_remove':
-        for group in instance.groups.all():
-            if group.name == 'stu' and hasattr(instance, 'studentprofile'):
-                instance.studentprofile.delete()
-            elif group.name == 'tch' and hasattr(instance, 'teacherprofile'):
-                instance.teacherprofile.delete()
+        with transaction.atomic():
+            for group in instance.groups.filter(pk__in=pk_set):
+                if group.name == 'stu' and hasattr(instance, 'studentprofile'):
+                    instance.studentprofile.delete()
+                elif group.name == 'tch' and hasattr(instance, 'teacherprofile'):
+                    instance.teacherprofile.delete()
